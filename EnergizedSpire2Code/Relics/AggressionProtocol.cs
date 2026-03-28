fcf2229e7f552be4945d135d67377c63f919b984
JohnnyBazooka89 <johnnybazooka89@gmail.com>
2026-03-28 17:26:37 +0100

    Add Aggression Protocol.

---
+++ EnergizedSpire2Code/Relics/AggressionProtocol.cs
@@ -0,0 +1,51 @@
+﻿#region
+
+using BaseLib.Utils;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.Entities.Players;
+using MegaCrit.Sts2.Core.Entities.Relics;
+using MegaCrit.Sts2.Core.HoverTips;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.Models.RelicPools;
+
+#endregion
+
+namespace EnergizedSpire2.EnergizedSpire2Code.Relics;
+
+[Pool(typeof(EventRelicPool))]
+public class AggressionProtocol : EnergizedSpire2Relic
+{
+    public override RelicRarity Rarity => RelicRarity.Ancient;
+
+    protected override IEnumerable<DynamicVar> CanonicalVars =>
+    [
+        new EnergyVar(1)
+    ];
+
+    protected override IEnumerable<IHoverTip> ExtraHoverTips =>
+    [
+        HoverTipFactory.ForEnergy(this)
+    ];
+
+    public override decimal ModifyMaxEnergy(Player player, decimal amount)
+    {
+        return player != Owner ? amount : amount + DynamicVars.Energy.IntValue;
+    }
+
+    public override bool ShouldPlay(CardModel card, AutoPlayType autoPlayType)
+    {
+        if (card.Owner != Owner)
+        {
+            return true;
+        }
+
+        IEnumerable<CardModel> attacks = CardPile.GetCards(Owner, PileType.Hand).Where(c => c.Type == CardType.Attack);
+        if (!attacks.Any())
+        {
+            return true;
+        }
+
+        return card.Type == CardType.Attack;
+    }
+}
