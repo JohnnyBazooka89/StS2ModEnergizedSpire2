0425b68b89c3e3f5fbbc1255f9ba8558ff08e7c2
JohnnyBazooka89 <johnnybazooka89@gmail.com>
2026-03-31 00:59:44 +0200

    Add Never-Ending Sparkler.

---
+++ EnergizedSpire2Code/Relics/NeverEndingSparkler.cs
@@ -0,0 +1,68 @@
+﻿using BaseLib.Utils;
+using MegaCrit.Sts2.Core.Entities.Players;
+using MegaCrit.Sts2.Core.Entities.Relics;
+using MegaCrit.Sts2.Core.HoverTips;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Map;
+using MegaCrit.Sts2.Core.Models.Powers;
+using MegaCrit.Sts2.Core.Models.RelicPools;
+using MegaCrit.Sts2.Core.Nodes.Screens.Map;
+using MegaCrit.Sts2.Core.Random;
+
+namespace EnergizedSpire2.EnergizedSpire2Code.Relics;
+
+[Pool(typeof(EventRelicPool))]
+public class NeverEndingSparkler : EnergizedSpire2Relic
+{
+    private const string ChancePercentKey = "ChancePercent";
+
+    public override RelicRarity Rarity => RelicRarity.Ancient;
+
+    protected override IEnumerable<DynamicVar> CanonicalVars =>
+    [
+        new EnergyVar(1),
+        new(ChancePercentKey, 50M),
+    ];
+
+    protected override IEnumerable<IHoverTip> ExtraHoverTips =>
+    [
+        HoverTipFactory.ForEnergy(this),
+        HoverTipFactory.FromPower<ThornsPower>()
+    ];
+
+    public override decimal ModifyMaxEnergy(Player player, decimal amount)
+    {
+        return player != Owner ? amount : amount + DynamicVars.Energy.IntValue;
+    }
+
+    public override Task AfterObtained()
+    {
+        AddMarkedRooms(Owner.RunState.Map);
+        return Task.CompletedTask;
+    }
+
+    public override Task AfterActEntered()
+    {
+        AddMarkedRooms(Owner.RunState.Map);
+        return base.AfterActEntered();
+    }
+
+    private ActMap AddMarkedRooms(ActMap map)
+    {
+        Rng rng = new Rng(Owner.RunState.Rng.Seed, 100 * Owner.RunState.ActFloor);
+        List<MapPoint> questionRooms = map.GetAllMapPoints().Where(p => p.PointType == MapPointType.Unknown)
+            .ToList();
+
+        foreach (MapPoint questionRoom in questionRooms)
+        {
+            if (rng.NextInt(100) < DynamicVars[ChancePercentKey].BaseValue)
+            {
+                questionRoom.PointType = MapPointType.Elite;
+            }
+        }
+
+        NMapScreen.Instance?.RefreshAllPointVisuals();
+
+        return map;
+    }
+}
