1fd959a352caf0cdbaf14b4029ae37ee9561c4c9
JohnnyBazooka89 <johnnybazooka89@gmail.com>
2026-03-31 18:52:05 +0200

    Add Spider Web.

---
+++ EnergizedSpire2Code/Relics/SpiderWeb.cs
@@ -0,0 +1,54 @@
+﻿using BaseLib.Utils;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.Entities.Players;
+using MegaCrit.Sts2.Core.Entities.Relics;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.HoverTips;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.Models.RelicPools;
+
+namespace EnergizedSpire2.EnergizedSpire2Code.Relics;
+
+[Pool(typeof(EventRelicPool))]
+public class SpiderWeb : EnergizedSpire2Relic
+{
+    private const string EnergyIncreaseCostKey = "EnergyIncreaseCost";
+
+    public override RelicRarity Rarity => RelicRarity.Ancient;
+
+    protected override IEnumerable<DynamicVar> CanonicalVars =>
+    [
+        new EnergyVar(1),
+        new EnergyVar(EnergyIncreaseCostKey, 1)
+    ];
+
+    protected override IEnumerable<IHoverTip> ExtraHoverTips =>
+    [
+        HoverTipFactory.ForEnergy(this)
+    ];
+
+    public override decimal ModifyMaxEnergy(Player player, decimal amount)
+    {
+        return player != Owner ? amount : amount + DynamicVars.Energy.IntValue;
+    }
+
+    public override Task AfterPlayerTurnStart(PlayerChoiceContext choiceContext, Player player)
+    {
+        if (player != Owner)
+        {
+            return Task.CompletedTask;
+        }
+
+        List<CardModel> attacks = PileType.Hand.GetPile(Owner).Cards.Where(c => c.Type == CardType.Attack).ToList();
+        if (attacks.Count == 0)
+        {
+            return Task.CompletedTask;
+        }
+
+        Flash();
+        Owner.RunState.Rng.Shuffle.Shuffle(attacks);
+        attacks[0].EnergyCost.AddThisTurn((int)DynamicVars[EnergyIncreaseCostKey].BaseValue);
+        return Task.CompletedTask;
+    }
+}
