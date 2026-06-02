cbae0b305c924bebb1a1d6a0ebbd2621e90d4f35
JohnnyBazooka89 <johnnybazooka89@gmail.com>
2026-06-02 23:27:44 +0200

    Change 'Spider Web' to use Entangled affliction.

--- EnergizedSpire2Code/Relics/SpiderWeb.cs
+++ EnergizedSpire2Code/Relics/SpiderWeb.cs
@@ -1,11 +1,15 @@
 ﻿using BaseLib.Utils;
+using MegaCrit.Sts2.Core.Combat;
+using MegaCrit.Sts2.Core.Commands;
 using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.Entities.Creatures;
 using MegaCrit.Sts2.Core.Entities.Players;
 using MegaCrit.Sts2.Core.Entities.Relics;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
 using MegaCrit.Sts2.Core.HoverTips;
 using MegaCrit.Sts2.Core.Localization.DynamicVars;
 using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.Models.Afflictions;
 using MegaCrit.Sts2.Core.Models.RelicPools;
 
 namespace EnergizedSpire2.EnergizedSpire2Code.Relics;
@@ -25,7 +29,8 @@ public class SpiderWeb : EnergizedSpire2Relic
 
     public override IEnumerable<IHoverTip> ExtraHoverTips =>
     [
-        HoverTipFactory.ForEnergy(this)
+        HoverTipFactory.ForEnergy(this),
+        ..HoverTipFactory.FromAffliction<Entangled>()
     ];
 
     public override decimal ModifyMaxEnergy(Player player, decimal amount)
@@ -33,22 +38,39 @@ public class SpiderWeb : EnergizedSpire2Relic
         return player != Owner ? amount : amount + DynamicVars.Energy.IntValue;
     }
 
-    public override Task AfterPlayerTurnStart(PlayerChoiceContext choiceContext, Player player)
+    public override async Task AfterPlayerTurnStart(PlayerChoiceContext choiceContext, Player player)
     {
         if (player != Owner)
         {
-            return Task.CompletedTask;
+            return;
         }
 
-        List<CardModel> attacks = PileType.Hand.GetPile(Owner).Cards.Where(c => c.Type == CardType.Attack).ToList();
+        List<CardModel> attacks = PileType.Hand.GetPile(Owner).Cards.Where(c => c is { Type: CardType.Attack, Affliction: null }).ToList();
         if (attacks.Count == 0)
         {
-            return Task.CompletedTask;
+            return;
         }
 
         Flash();
         Owner.RunState.Rng.Shuffle.Shuffle(attacks);
         attacks[0].EnergyCost.AddThisTurn(DynamicVars[EnergyCostIncreaseKey].IntValue);
+        await CardCmd.Afflict<Entangled>(attacks[0], 1M);
+    }
+    
+    
+    public override Task AfterSideTurnEnd(
+        PlayerChoiceContext choiceContext,
+        CombatSide side,
+        IEnumerable<Creature> participants)
+    {
+        if (!participants.Contains(Owner.Creature))
+            return Task.CompletedTask;
+
+        foreach (CardModel card in Owner.PlayerCombatState?.AllCards.Where(c => c.Affliction is Entangled) ?? [])
+        {
+            CardCmd.ClearAffliction(card);
+        }
+
         return Task.CompletedTask;
     }
 }
