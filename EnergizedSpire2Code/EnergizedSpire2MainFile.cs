0eda4c21d83268239961ce268d64c514d679e303
JohnnyBazooka89 <johnnybazooka89@gmail.com>
2026-07-28 19:52:50 +0200

    Use resource path correctly for images and scenes.

--- EnergizedSpire2Code/EnergizedSpire2MainFile.cs
+++ EnergizedSpire2Code/EnergizedSpire2MainFile.cs
@@ -9,7 +9,8 @@ namespace EnergizedSpire2.EnergizedSpire2Code;
 [ModInitializer(nameof(Initialize))]
 public partial class EnergizedSpire2MainFile : Node
 {
-    public const string ModId = "EnergizedSpire2"; //Used for resource filepath
+    private const string ModId = "EnergizedSpire2"; //Used for resource filepath
+    public const string ResPath = $"res://{ModId}";
 
     public static Logger Logger { get; } = new(ModId, LogType.Generic);
 
--- EnergizedSpire2Code/Extensions/StringExtensions.cs
+++ EnergizedSpire2Code/Extensions/StringExtensions.cs
@@ -5,51 +5,51 @@ public static class StringExtensions
 {
     public static string ImagePath(this string path)
     {
-        return Path.Join(EnergizedSpire2MainFile.ModId, "images", path);
+        return Path.Join(EnergizedSpire2MainFile.ResPath, "images", path);
     }
 
     public static string CardImagePath(this string path)
     {
-        return Path.Join(EnergizedSpire2MainFile.ModId, "images", "card_portraits", path);
+        return Path.Join(EnergizedSpire2MainFile.ResPath, "images", "card_portraits", path);
     }
 
     public static string BetaCardImagePath(this string path)
     {
-        return Path.Join(EnergizedSpire2MainFile.ModId, "images", "card_portraits", "beta", path);
+        return Path.Join(EnergizedSpire2MainFile.ResPath, "images", "card_portraits", "beta", path);
     }
     
     public static string BigCardImagePath(this string path)
     {
-        return Path.Join(EnergizedSpire2MainFile.ModId, "images", "card_portraits", "big", path);
+        return Path.Join(EnergizedSpire2MainFile.ResPath, "images", "card_portraits", "big", path);
     }
 
     public static string PowerImagePath(this string path)
     {
-        return Path.Join(EnergizedSpire2MainFile.ModId, "images", "powers", path);
+        return Path.Join(EnergizedSpire2MainFile.ResPath, "images", "powers", path);
     }
 
     public static string BigPowerImagePath(this string path)
     {
-        return Path.Join(EnergizedSpire2MainFile.ModId, "images", "powers", "big", path);
+        return Path.Join(EnergizedSpire2MainFile.ResPath, "images", "powers", "big", path);
     }
 
     public static string RelicImagePath(this string path)
     {
-        return Path.Join(EnergizedSpire2MainFile.ModId, "images", "relics", path);
+        return Path.Join(EnergizedSpire2MainFile.ResPath, "images", "relics", path);
     }
 
     public static string RelicOutlineImagePath(this string path)
     {
-        return Path.Join(EnergizedSpire2MainFile.ModId, "images", "relics", "outline", path);
+        return Path.Join(EnergizedSpire2MainFile.ResPath, "images", "relics", "outline", path);
     }
 
     public static string BigRelicImagePath(this string path)
     {
-        return Path.Join(EnergizedSpire2MainFile.ModId, "images", "relics", "big", path);
+        return Path.Join(EnergizedSpire2MainFile.ResPath, "images", "relics", "big", path);
     }
 
     public static string AncientImagePath(this string path)
     {
-        return Path.Join(EnergizedSpire2MainFile.ModId, "images", "ancients", path);
+        return Path.Join(EnergizedSpire2MainFile.ResPath, "images", "ancients", path);
     }
 }
